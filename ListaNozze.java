eca821516e8f2514d1f16570f3e1080daea46513
MandichRiccardo <ricky.mandich@gmail.com>
2023-12-05 13:34:07 +0100

    creazione 2023 12 05 12:59 creazione lista nozze

--- ListaNozze.java
+++ ListaNozze.java
@@ -1,9 +1,11 @@
+import java.util.Arrays;
+
 public class ListaNozze{
     protected Sposi coniugi;
     protected int maxArticoli;
     protected String negozio;
     protected Articolo[] articoliDesiderati;
-    protected int prezzoTot;
+    protected double prezzoTot;
     protected Articolo[] articoliRegalati;
 
     public ListaNozze(Sposi coniugi, int maxArticoli, String negozio, Articolo[] articoliDesiderati) {
@@ -28,31 +30,53 @@ public class ListaNozze{
         return negozio;
     }
 
-    public void setNegozio(String negozio) {
-        this.negozio = negozio;
+    public String getArticoliDesiderati() {
+        String info = "";
+        for(int i=0;i<articoliDesiderati.length;i++){
+            info = info.concat(articoliDesiderati[i].toString());
+        }
+        return info;
     }
 
-    public Articolo[] getArticoliDesiderati() {
-        return articoliDesiderati;
+    public String aggiungiDesiderata(Articolo nuovoArticolo) {
+        if(articoliDesiderati[maxArticoli-1] != null) return "la lista nozze è piena";
+        int i=0;
+        while(articoliDesiderati[i]!=null){
+            if(articoliDesiderati[i].equals(nuovoArticolo)) return "questo articolo è già presente";
+            i++;
+        }
+        articoliDesiderati[i] = nuovoArticolo;
+        setPrezzoTot();
+        return "ho aggiunto questo articolo";
     }
 
-    public void setArticoliDesiderati(Articolo[] articoliDesiderati) {
-        this.articoliDesiderati = articoliDesiderati;
-    }
-
-    public int getPrezzoTot() {
+    public double getPrezzoTot() {
         return prezzoTot;
     }
 
-    public void setPrezzoTot(int prezzoTot) {
-        this.prezzoTot = prezzoTot;
+    protected void setPrezzoTot() {
+        prezzoTot = 0;
+        for(int i=0;i<articoliDesiderati.length;i++){
+            if(articoliDesiderati[i] != null) prezzoTot += articoliDesiderati[i].costo;
+        }
     }
 
-    public Articolo[] getArticoliRegalati() {
-        return articoliRegalati;
+    public String getArticoliRegalati() {
+        String info = "";
+        for(int i=0;i<articoliRegalati.length;i++){
+            info = info.concat(articoliRegalati[i].toString());
+        }
+        return info;
     }
 
-    public void setArticoliRegalati(Articolo[] articoliRegalati) {
-        this.articoliRegalati = articoliRegalati;
+    public String toString() {
+        String info = "";
+        info += "coniugi:\t" + coniugi + "\n";
+        info += "articoli massimi:\t" + maxArticoli + "\n";
+        info += "negozio:\t" + negozio + "\n";
+        info += getArticoliDesiderati();
+        info += "prezzo totale:\t" + prezzoTot + "\n";
+        info += getArticoliRegalati();
+        return info;
     }
 }
