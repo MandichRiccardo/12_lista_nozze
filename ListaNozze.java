8f068f21038f12afe1905f92e1b0561f1d0661b8
MandichRiccardo <ricky.mandich@gmail.com>
2023-12-05 16:34:24 +0100

    aggiornamento 2023 12 05 16:34 programma completo, manca solo il main

--- ListaNozze.java
+++ ListaNozze.java
@@ -55,6 +55,23 @@ public class ListaNozze{
         return info;
     }
 
+    public String setArticoliRegalati(Articolo a, int quantita) {
+        int i=0;
+        while(i<articoliRegalati.length){
+            if(articoliRegalati[i]!=null){
+                if(articoliRegalati[i].equals(a)){
+                    articoliRegalati[i] = a;
+                    return "ho aggiunto l'articolo";
+                }
+                i++;
+            }else{
+                articoliRegalati[i] = a;
+                return "ho aggiunto l'articolo";
+            }
+        }
+        return "l'elenco è pieno, non posso aggiungere altri articoli";
+    }
+
     public String toString() {
         String info = "";
         info += "coniugi:\t" + coniugi + "\n";
@@ -76,5 +93,26 @@ public class ListaNozze{
         articoliDesiderati[i] = nuovoArticolo;
         setPrezzoTot();
         return "ho aggiunto questo articolo";
+    }
+    protected String rimuoviArticoloDesiderato(int j){
+        if(j>=articoliDesiderati.length) return "sei fuori dall'elenco nozze";
+        if(articoliDesiderati[j] == null) return "questo oggetto non esiste";
+        int i;
+        for (i=j;i<articoliDesiderati.length-1;i++) articoliDesiderati[i] = articoliDesiderati[i+1];
+        articoliDesiderati[i] = null;
+        return "ho eliminato l'articolo";
+    }
+    public void compraRegalo(){
+        int i=Interazione.input(getArticoliDesiderati() + "\nquesti sono gli articoli presenti nella lista nozze di " + coniugi.getSposo() + " e " + coniugi.getSposa() + ", inserisci il numero di fianco all'articolo che gli vuoi regalare");
+        int quantita = 0;
+        if(articoliDesiderati[i].quantita != 1){
+            while(quantita<1||quantita>articoliDesiderati[i].quantita) quantita = Interazione.input("quanti di questo articolo vuoi regalargli?\t(max " + articoliDesiderati[i].quantita + " e min 1)");
+        }
+        articoliDesiderati[i].quantita -= quantita;
+        setArticoliRegalati(articoliDesiderati[i], quantita);
+        if(articoliDesiderati[i].quantita == 0){
+            rimuoviArticoloDesiderato(i);
+        }
+        setPrezzoTot();
     }
 }
